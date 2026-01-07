2c7723d704a976f4d702bf2a4c58678724b1baae
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-01-07 18:33:28 +0000

    add config ref

--- modules/ly.nix
+++ modules/ly.nix
@@ -1,11 +1,9 @@
 {
-  inputs,
-  config,
-  pkgs,
   ...
 }:
 
 {
+  # https://codeberg.org/fairyglade/ly/src/branch/master/res/config.ini
   services.displayManager.ly = {
     enable = true;
     settings = {
