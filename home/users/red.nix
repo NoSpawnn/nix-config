a54a1526af1a876af6ba2e2b1451f6aa6a75dd24
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-01-10 21:27:10 +0000

    add chezmoi integration

--- home/users/red.nix
+++ home/users/red.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   ...
 }:
 
@@ -26,11 +27,23 @@
     nil
     tealdeer
     zellij
+    chezmoi
   ];
 
   home.sessionVariables = {
     "EDITOR" = "hx";
   };
+
+  # idk if we actually care about resetting the path but whatever
+  home.activation.chezmoi = lib.hm.dag.entryAfter [ "installPackages" ] ''
+    _path="$PATH"
+
+    PATH="${pkgs.chezmoi}/bin:$PATH"
+    run chezmoi init git.nospawnn.com/red/dotfiles
+    run chezmoi update -a
+
+    PATH="$_path"
+  '';
 
   programs.home-manager.enable = true;
   home.stateVersion = "25.11";
