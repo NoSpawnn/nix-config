89f23f4aabfc4e7ea9348ba1515491a45fba8be0
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-01-11 16:33:27 +0000

    cleanup niri module

--- modules/niri.nix
+++ modules/niri.nix
@@ -7,9 +7,10 @@
 }:
 
 let
-  noctalia-shell = flake-inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default;
-  niri-scratchpad =
-    flake-inputs.niri-scratchpad.packages.${pkgs.stdenv.hostPlatform.system}.niri-scratchpad;
+  extraPkgs = with flake-inputs; {
+    noctalia-shell = noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    niri-scratchpad = niri-scratchpad.packages.${pkgs.stdenv.hostPlatform.system}.niri-scratchpad;
+  };
 in
 {
   imports = [
@@ -17,33 +18,34 @@ in
   ];
 
   programs.niri.enable = true;
-  environment.systemPackages = with pkgs; [
-    # core for niri
-    alacritty
-    fuzzel
-    nautilus
-    xdg-desktop-portal
-    xdg-desktop-portal-gnome
-    xdg-desktop-portal-gtk
-    gnome-keyring
-    polkit
-    polkit_gnome
-    xwayland-satellite
-    swaybg
-    swaylock
-    niri-scratchpad
-    noctalia-shell
+  environment.systemPackages =
+    with pkgs;
+    [
+      # core for niri
+      alacritty
+      fuzzel
+      nautilus
+      xdg-desktop-portal
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+      gnome-keyring
+      polkit
+      polkit_gnome
+      xwayland-satellite
+      swaybg
+      swaylock
 
-    # additional stuff
-    # quickshell # noctalia-shell installs this for us
-    wlsunset
-    brightnessctl
-    gpu-screen-recorder
-    pavucontrol
+      # additional stuff
+      # quickshell # noctalia-shell installs this for us
+      wlsunset
+      brightnessctl
+      gpu-screen-recorder
+      pavucontrol
 
-    # for config
-    kdlfmt
-  ];
+      # for config
+      kdlfmt
+    ]
+    ++ (builtins.attrValues extraPkgs);
 
   security.polkit.enable = true;
   systemd.user.services.polkit-gnome-authentication-agent-1 = {
