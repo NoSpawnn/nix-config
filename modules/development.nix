cf92021b337a1fbc89ba4ca1451bcaf4c2fbc0dc
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-04-12 22:29:36 +0100

    open incusbr0 ports

--- modules/development.nix
+++ modules/development.nix
@@ -15,5 +15,7 @@ in
       networking.nftables.enable = true;
       virtualisation.incus.enable = true;
       environment.systemPackages = [ pkgs.aardvark-dns ];
+      networking.firewall.interfaces.incusbr0.allowedTCPPorts = [ 53 67 ];
+      networking.firewall.interfaces.incusbr0.allowedUDPPorts = [ 53 67 ];
   };
 }
