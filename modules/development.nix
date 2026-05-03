36ef00f5a84dee116fdb6e01dcdb99b8e957c1dc
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-05-03 11:53:51 +0100

    disable incus

--- modules/development.nix
+++ modules/development.nix
@@ -13,15 +13,15 @@ in
     virtualisation.docker.enable = true;
     virtualisation.podman.enable = true;
     networking.nftables.enable = true;
-    virtualisation.incus.enable = true;
+    # virtualisation.incus.enable = true;
     environment.systemPackages = [ pkgs.aardvark-dns ];
-    networking.firewall.interfaces.incusbr0.allowedTCPPorts = [
-      53
-      67
-    ];
-    networking.firewall.interfaces.incusbr0.allowedUDPPorts = [
-      53
-      67
-    ];
+    # networking.firewall.interfaces.incusbr0.allowedTCPPorts = [
+    #   53
+    #   67
+    # ];
+    # networking.firewall.interfaces.incusbr0.allowedUDPPorts = [
+    #   53
+    #   67
+    # ];
   };
 }
