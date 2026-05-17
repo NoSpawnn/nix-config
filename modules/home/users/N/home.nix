ee965f40bd206204a8b5032f75ef45c6073858f3
Red <78027161+NoSpawnn@users.noreply.github.com>
2026-05-17 14:12:51 +0100

    impl nix-flatpak

--- modules/home/users/N/home.nix
+++ modules/home/users/N/home.nix
@@ -1,8 +1,27 @@
-{ pkgs, ... }:
+{ pkgs, flake-inputs, ... }:
+
 {
   imports = [
     ../../modules/wayland.nix
+    flake-inputs.nix-flatpak.homeManagerModules.nix-flatpak
   ];
+
+  services.flatpak = {
+    uninstallUnmanaged = true;
+    remotes = [
+      {
+        name = "flathub-user";
+        location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      }
+    ];
+    packages = [
+      "com.bitwarden.desktop"
+      "com.rtosta.zapzap"
+      "md.obsidian.Obsidian"
+      "org.onlyoffice.desktopeditors"
+      "org.signal.Signal"
+    ];
+  };
 
   home.username = "N";
   home.homeDirectory = "/home/N";
--- modules/profiles/pc.nix
+++ modules/profiles/pc.nix
@@ -1,17 +1,14 @@
 { pkgs, flake-inputs, ... }:
 
-let
-  zen-browser = flake-inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default;
-in
 {
   imports = [
     ./base.nix
 
     ../modules/gui/niri.nix
-    # ../modules/gui/kde.nix
-    # ../modules/gui/cosmic.nix
     ../modules/gaming.nix
     ../modules/development.nix
+
+    flake-inputs.nix-flatpak.nixosModules.nix-flatpak
   ];
 
   # Enable audio
@@ -20,17 +17,28 @@ in
     pulse.enable = true;
   };
 
-  environment.systemPackages =
-    with pkgs;
-    [
-      tor-browser
-      blueman
-      corefonts # ms-corefonts
-    ]
-    ++ [ zen-browser ];
+  environment.systemPackages = with pkgs; [
+    tor-browser
+    blueman
+    corefonts # ms-corefonts
+  ];
 
-  # Other package formats
-  services.flatpak.enable = true;
+  services.flatpak = {
+    enable = true;
+    uninstallUnmanaged = true;
+    remotes = [
+      # TODO: pr to nix-flatpak to allow changing the default origin, as this one MUST be named flathub, else you want to define `origin` for every package
+      {
+        name = "flathub";
+        location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+      }
+    ];
+    packages = [
+      "com.github.tchx84.Flatseal"
+      "app.zen_browser.zen"
+    ];
+  };
+
   programs.appimage = {
     enable = true;
     binfmt = true;
