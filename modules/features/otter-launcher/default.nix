fa9d9c56b7643f000eda4f9ddf4e93be6364d717
Red <me@nospawnn.com>
2026-08-05 22:45:14 +0100

    add fsel to otter-launcher pkgs

--- modules/features/otter-launcher/default.nix
+++ modules/features/otter-launcher/default.nix
@@ -14,6 +14,7 @@
     packages.otter-launcher = inputs.wrappers.lib.wrapPackage {
       inherit pkgs;
       package = inputs'.otter-launcher.packages.default;
+      runtimePkgs = [ pkgs.fsel ];
       flags."--config" = "${dotfiles}/dots/dot-config/otter-launcher/config.toml";
     };
   };
