ed7239a2786e2737b40c2c1ce9e816aecd46d1cf
Red <me@nospawnn.com>
2026-08-08 09:18:16 +0100

    make nemo use papirus icons
    WIP, might change these to something else, but Papirus is the only ones
    I could get to work...

--- modules/features/nemo/default.nix
+++ modules/features/nemo/default.nix
@@ -1,10 +1,24 @@
-{ moduleWithSystem, ... }:
+{ inputs, moduleWithSystem, ... }:
 
 {
   flake.nixosModules.nemo = moduleWithSystem (
     { self', ... }: { environment.systemPackages = [ self'.packages.nemo ]; }
   );
 
-  perSystem = { pkgs, ... }: { packages.nemo = pkgs.nemo; };
+  perSystem = { pkgs, ... }: {
+    packages.nemo = inputs.wrappers.lib.wrapPackage {
+      inherit pkgs;
+      package = pkgs.nemo;
 
+      env."GTK_ICON_THEME" = "Papirus";
+      env."XDG_ICON_THEME" = "Papirus";
+      prefixVar = [
+        [
+          "XDG_DATA_DIRS"
+          ":"
+          "${pkgs.papirus-icon-theme}/share"
+        ]
+      ];
+    };
+  };
 }
