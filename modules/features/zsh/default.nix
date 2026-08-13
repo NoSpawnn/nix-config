90f02b3fe8db2408dfa414368029befce98eb698
Red <me@nospawnn.com>
2026-08-13 08:04:57 +0100

    add some stuff to zsh config

--- modules/features/zsh/default.nix
+++ modules/features/zsh/default.nix
@@ -6,33 +6,37 @@
 }:
 
 {
-  flake.nixosModules.zsh =
-    let
-      flakePath = builtins.getEnv "PWD";
-    in
-    moduleWithSystem (
-      { self', pkgs, ... }: {
-        nixpkgs.overlays = [ (final: prev: { zsh = self'.packages.zsh; }) ];
-        programs.zsh = {
-          enable = true;
-          promptInit = (builtins.readFile "${dotfiles}/dots/dot-zshrc"); # + "prompt off";
-          shellAliases = {
-            "nrs" = "sudo nixos-rebuild switch --flake ${flakePath}";
-          };
+  flake.nixosModules.zsh = moduleWithSystem (
+    { self', pkgs, ... }: {
+      users.defaultUserShell = pkgs.zsh;
+
+      nixpkgs.overlays = [ (final: prev: { zsh = self'.packages.zsh; }) ];
+      programs.zsh = {
+        enable = true;
+        promptInit = builtins.readFile "${dotfiles}/dots/dot-zshrc";
+        shellAliases = {
+          "nrs" = "sudo nixos-rebuild switch --flake";
+          "nfu" = "nix flake update";
+
+          # TODO: maybe put these in zshrc guarded behing _command_exists?;
+          "nsh" = "nix-shell -p";
         };
-        users.defaultUserShell = pkgs.zsh;
-      }
-    );
+      };
+    }
+  );
 
   perSystem = { pkgs, self', ... }: {
     packages.zsh = inputs.wrappers.wrappers.zsh.wrap {
       inherit pkgs;
+      env."STARSHIP_CONFIG" = "${dotfiles}dots/dot-config/starship.toml";
       runtimePkgs = with pkgs; [
         self'.packages.nvim
+        yazi
         starship
         fzf
         fd
       ];
+
     };
   };
 }
