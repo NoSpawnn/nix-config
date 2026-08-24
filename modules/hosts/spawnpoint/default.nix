1797401b1144d5d13c7e169d183ee98ef1ad756e
Red <me@nospawnn.com>
2026-08-24 18:56:00 +0100

    fix spawnpoint config

--- modules/hosts/spawnpoint/default.nix
+++ modules/hosts/spawnpoint/default.nix
@@ -1,19 +1,14 @@
-{ self, inputs, ... }: {
-  flake.nixosConfigurations.spawnpoint = inputs.nixpkgs.lib.nixosSystem {
-    modules =
-      with self.nixosModules;
-      [
-        userN
-
-        amdDrivers
-        desktop
-        gaming
-
-        ({ ... }: {
-          networking.hostName = "spawnpoint";
-          networking.interfaces."enp11s0".wakeOnLan.enable = true;
-        })
-      ]
-      ++ [ ./_hardware-configuration.nix ];
+{ self, utils, ... }: {
+  flake.nixosConfigurations.spawnpoint = utils.mkNixosSystem {
+    hostname = "spawnpoint";
+    keymap = "us";
+    modules = with self.nixosModules; [
+      userN
+      desktop
+      gaming
+      amdDrivers
+      ./_hardware-configuration.nix
+    ];
   };
 }
+
