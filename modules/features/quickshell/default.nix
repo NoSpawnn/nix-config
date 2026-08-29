e9ec55303e0940716c8d15e13cbb09884cfd17fc
Red <me@nospawnn.com>
2026-08-29 09:49:33 +0100

    init quickshell

---
+++ modules/features/quickshell/default.nix
@@ -0,0 +1,13 @@
+{ inputs, dotfiles, moduleWithSystem, ... }: {
+  flake.nixosModules.quickshell = moduleWithSystem (
+    { self', ... }: { environment.systemPackages = [ self'.packages.quickshell ]; }
+  );
+
+  perSystem = { pkgs, ... }: {
+    packages.quickshell = inputs.wrappers.lib.wrapPackage {
+      inherit pkgs;
+      package = pkgs.quickshell;
+      flags."--path" = "${dotfiles}/dots/dot-config/quickshell";
+    };
+  };
+}
